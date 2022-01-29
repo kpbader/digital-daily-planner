c5be92096f7ece91ae3869b1be9f91efa2b29bf4
Kyle Patrick Bader <kbader91@gmail.com>
2022-01-29 13:55:18 -0800

    final touches in develop

--- script.js
+++ script.js
@@ -1,46 +1,42 @@
 
-// WHEN I open the planner
-// THEN the current day is displayed at the top of the calendar
-var currentTimeEl = new Date();
+// displays current time 
+var currentTimeEl = moment().format('MMMM Do YYYY, h:mm:ss a');
 document.getElementById("currentDay").textContent = currentTimeEl;
 
+// save items
+$('.saveBtn').on('click', function (){
+    var value= $(this).siblings('.description').val();
+    var key = $(this).parent().attr('id')
 
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future 
-
-// // check due date
-// auditTask(taskLi);
-
- var auditTask = function(taskEl) {
-        // get date from task element 
-        var date = $(taskEl).find("span").text().trim();
-       
-        // convert to moment object at 5:00pm
-        var time = moment(date, "L").set("hour", 17);
-        
-        // remove any old classes from element 
-        $(taskEl).removeClass("list-group-item-warning list-group-item-danger");
-       
-        // apply new class if task is near/over due date 
-        if (moment().isAfter(currentTimeEl)) {
-          $(taskEl).addClass("list-group-item-danger");
-         }
-         else if (Math.abs(moment().diff(time, "days")) <= 2) {
-           $(taskEl).addClass("list-group-item-warning");
-         } 
- };
+    localStorage.setItem(key, value)
+});
 
 
-// WHEN I click the save button for that time block 
-// THEN the text for that event is saved in local storage 
-var saveTasks = function() {
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-};
+// // check due date (will change color according to time)
+function auditTime() {
+    var currentHour = moment().hour();
 
+    $('.time-block').each(function(){
+        var temp = parseInt($(this).attr("id"))
+        if (currentHour === temp) {
+                $(this).addClass("present");
+            } else if (currentHour > temp) {
+                $(this).addClass("past");
+            }else{
+                $(this).addClass("future");
+            }
+    })
+}
+auditTime();
 
-// WHEN I refresh the page 
-// THEN the saved events persist 
-var loadTasks = function() {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-  };
-  
+// load items to page
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
+
